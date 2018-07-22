90b811ed00c33d465d51062d530e0ab2bbc441d2
Vlad Ciphersky <97faizov@gmail.com>
2018-07-22 12:17:43 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 });
 
 client.on("message", async message => {
-  console.log(message.channel.name + ` | ` + message.author.tag + ` (${message.author.ID}): ` + message.content);
+  console.log(`#` + message.channel.name + ` | ` + message.author.tag + ` (` + message.author.id + `): ` + message.content);
   if(message.author.bot) return;
   if(message.content.indexOf(process.env.PREFIX) !== 0) return;
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
